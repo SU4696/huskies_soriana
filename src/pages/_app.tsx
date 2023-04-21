8958609e40f61eac141743edbd969077fa356605
suyeon4696 <89484736+SU4696@users.noreply.github.com>
2023-04-20 22:58:57 -0600

    login & crear cuenta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 import Footer from "@/components/Footer";
+import { AuthContextProvider } from "@/context/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-      <Footer />
-    </ChakraProvider>
+    <AuthContextProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+        <Footer />
+      </ChakraProvider>
+    </AuthContextProvider>
   );
 }
