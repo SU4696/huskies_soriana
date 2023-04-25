211c3e070c993daaad2567657fc0bc8652fb813c
Tlanetzi Chavez <a00830340@itesm.mx>
2023-04-24 23:55:35 -0600

    Botones arreglados, inicio del carrusel

--- src/components/ContenidoInicio.tsx
+++ src/components/ContenidoInicio.tsx
@@ -1,3 +1,4 @@
+import Carrusel from "./Carrusel"
 
 
 const ContenidoInicio = () => {
@@ -5,27 +6,27 @@ const ContenidoInicio = () => {
     <div className="bg-white">
         <p>Imagen</p>
         <div className="bg-primary ">
-            <p className="text-center">Carrusel</p>
-            <p className="sm:text-xs lg:text-lg mt-5 mb-5 text-center font-bold">Recomendados</p>
-                <div className="sm:flex-1 md:justify-center md:flex space-x-5">
+            <Carrusel/>
+            <p className=" lg:text-lg mt-5 mb-5 text-center font-bold">Recomendados</p>
+                <div className="justify-center flex space-x-2 md:space-x-5 px-5">
                     <button
                         type="button"
-                        className="py-2 px-5 text-sm bg-secondary text-white font-medium uppercase rounded-lg  hover:bg-green-800"
+                        className="py-1 px-1 md:py-2 md:px-5 text-sm bg-secondary text-white md:font-medium uppercase rounded-lg  hover:bg-green-800"
                     >Todos</button>
 
                     <button
                         type="button"
-                        className="py-2 px-5 text-sm bg-gray-400 text-black font-medium uppercase rounded-lg  hover:bg-gray-500"
+                        className="py-1 px-1 md:py-2 md:px-5 text-sm bg-gray-400 text-black md:font-medium uppercase rounded-lg  hover:bg-gray-500"
                     >Más nuevos</button>
 
                     <button
                         type="button"
-                        className="py-2 px-5 text-sm bg-gray-400 text-black font-medium uppercase rounded-lg  hover:bg-gray-500"
+                        className="py-1 px-1 md:py-2 md:px-5 text-sm bg-gray-400 text-black md:font-medium uppercase rounded-lg  hover:bg-gray-500"
                     >Popular</button>
 
                     <button
                         type="button"
-                        className="py-2 px-5 text-sm bg-gray-400 text-black font-medium uppercase rounded-lg  hover:bg-gray-500"
+                        className="py-1 px-1 md:py-2 md:px-5 text-sm bg-gray-400 text-black md:font-medium uppercase rounded-lg  hover:bg-gray-500"
                     >En Tendencia</button>
 
                 </div>
