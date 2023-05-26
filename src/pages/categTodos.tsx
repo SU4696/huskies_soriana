a6c421b01210a4c157e827f3c58363e846ebba54
suyeon4696 <89484736+SU4696@users.noreply.github.com>
2023-05-25 18:43:39 -0600

    categTodos:paddingtop for fix topbar

--- src/pages/categTodos.tsx
+++ src/pages/categTodos.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import TopbarCateg from '@/components/TopbarCateg';
-import Categoria from '@/components/Categoria';
-import ProductoCard from '@/components/ProductoCard';
+import React from "react";
+import TopbarCateg from "@/components/TopbarCateg";
+import Categoria from "@/components/Categoria";
+import ProductoCard from "@/components/ProductoCard";
+import { Box } from "@chakra-ui/react";
 
 function main() {
   return (
     <React.Fragment>
-        <TopbarCateg titulo={"Todos"} />
+      <TopbarCateg titulo={"Todos"} />
+      <Box paddingTop={"62px"}>
         <Categoria />
+      </Box>
     </React.Fragment>
-  )
+  );
 }
 
-export default main
+export default main;
